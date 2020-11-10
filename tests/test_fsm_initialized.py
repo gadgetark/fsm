77c21e6defa398b7c87028fc2ee3d8ac681f2f94
kchong <kengyjoe.chong@gmail.com>
2020-11-10 06:59:57 -0600

    Added full fsm transitions tests.

--- tests/test_fsm_initialized.py
+++ tests/test_fsm_initialized.py
@@ -6,7 +6,7 @@ import requests
 import json
 
 
-def test_fsm_initialized():
+def test_default_fsm_initialized():
     url = 'http://127.0.0.1:5000/fsm'
     headers = {'Content-Type': 'application/json'}
 
@@ -44,6 +44,67 @@ def test_fsm_initialized():
     test_post_action_name("action_1")
     test_fsm_current_state("state_b")
     test_fsm_valid_actions(["action_3"])
+
+
+def test_full_fsm_initialized():
+    url = 'http://127.0.0.1:5000/fsm'
+    headers = {'Content-Type': 'application/json'}
+
+    # Body
+    payload = {
+        "start": "state_a",
+        "transitions": [
+            {
+                "currentState": "state_a",
+                "nextState": "state_b",
+                "actionName": "action_1"
+            },
+            {
+                "currentState": "state_a",
+                "nextState": "state_c",
+                "actionName": "action_2"
+            },
+            {
+                "currentState": "state_b",
+                "nextState": "state_c",
+                "actionName": "action_3"
+            },
+            {
+                "currentState": "state_b",
+                "nextState": "state_a",
+                "actionName": "action_5"
+            },
+            {
+                "currentState": "state_c",
+                "nextState": "state_a",
+                "actionName": "action_4"
+            },
+            {
+                "currentState": "state_c",
+                "nextState": "state_b",
+                "actionName": "action_6"
+            }
+        ]
+    }
+
+    resp = requests.post(url, headers=headers, data=json.dumps(payload, indent=4))
+
+    assert resp.status_code == 201
+
+    print(resp.text)
+
+    test_fsm_current_state("state_a")
+    test_fsm_valid_actions(["action_1", "action_2"])
+
+    test_fsm_current_state("state_a")
+    test_post_action_name("action_1")
+    test_fsm_current_state("state_b")
+    test_fsm_valid_actions(["action_3", "action_5"])
+
+    test_fsm_current_state("state_b")
+    test_post_action_name("action_3")
+    test_fsm_current_state("state_c")
+    test_fsm_valid_actions(["action_4", "action_6"])
 
 
 def test_fsm_current_state(expected_current_state):
