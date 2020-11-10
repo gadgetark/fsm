d595fbf6e49420e6d3487f76058c6e0b57bf8cb0
kchong <kengyjoe.chong@gmail.com>
2020-11-09 22:20:54 -0600

    initialize fsm in the main.py

--- main.py
+++ main.py
@@ -18,6 +18,10 @@
 import json
 
 import flask
+from flask import request
+
+from fsm import FiniteStateMachine
+from fsm import State
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
@@ -30,9 +34,25 @@ def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 
-@app.route('/companies', methods=['POST'])
-def post_companies():
-    return json.dumps({"success": True}), 201
+@app.route('/fsm', methods=['POST'])
+def initialize_fsm():
+    req_data = request.get_json()
+    print("start:" + req_data['start'])
+    print("transitions:" + json.dumps(req_data['transitions']))
+
+    # return json.dumps({"success": True}), 201
+
+    print("PRINTING:")
+    # print(req_data['transitions'][0]['currentState'])
+    fsm = FiniteStateMachine(State(req_data['start']))
+
+    for transition in req_data['transitions']:
+        print("One transition's currentState:" + transition['currentState'])
+        fsm.add_transition(transition['currentState'],
+                           transition['nextState'],
+                           transition['actionName'])
+    return json.dumps(req_data), 201
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()  # run our Flask app
