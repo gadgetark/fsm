eeab32e02d72c4b8a15c9b9986a99dbbade56686
kchong <kengyjoe.chong@gmail.com>
2020-11-09 21:26:23 -0600

    Changed the fsm to assume and handle there is a state required and provided during initialization.

--- fsm.py
+++ fsm.py
@@ -32,15 +32,22 @@ class State:
 class IFiniteStateMachine:
     state_objects = {}
 
-    def __init__(self, current_state):
-        self.current_state = current_state
+    def __init__(self, start_state):
+        # when initializing it is safe to assume the current_state is the start_state
+        self.current_state = start_state
 
     def add_transition(self, current_state_name, next_state_name, action_name):
         # self.current_state_name = current_state_name
         new_transition = Transition(current_state_name, next_state_name, action_name)
-        new_state = State(current_state_name)
+        new_state = self.create_new_state_if_none(current_state_name)
         new_state.add_transition(new_transition)
         self.state_objects[current_state_name] = new_state
+
+    def create_new_state_if_none(self, current_state_name):
+        if current_state_name in self.state_objects.keys():
+            return self.state_objects[current_state_name]
+        else:
+            return State(current_state_name)
 
     def perform_action(self, action_name):
         if self.current_state.is_action_name_valid(action_name):
